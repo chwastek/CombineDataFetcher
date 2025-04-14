d0c6e3ee37f30b2f32e2e9ad90a77328f72598f6
Filip Chwastowski <chwastek@gmail.com>
2025-04-14 15:56:02 +0200

    Created Xcode project.

---
+++ CombineDataFetcher/CombineDataFetcher/CombineDataFetcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineDataFetcherApp.swift
+//  CombineDataFetcher
+//
+//  Created by Chwastek on 14/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct CombineDataFetcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CombineDataFetcher/CombineDataFetcherTests/CombineDataFetcherTests.swift
@@ -0,0 +1,16 @@
+//
+//  CombineDataFetcherTests.swift
+//  CombineDataFetcherTests
+//
+//  Created by Chwastek on 14/04/2025.
+//
+
+import Testing
+
+struct CombineDataFetcherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
